9ea45c1db2d081e7a8dace3771ee03c47f92478b
Tira Sundara <sundaralinus@gmail.com>
2017-10-24 13:51:11 +0700

    db:seed and adding some style

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,43 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Memastikan bahwa class Food bersih
+Food.delete_all
+
+Food.create!(
+  name: "Bakso Malang",
+  description:
+  %{
+    <p>
+    <em>Bakso ter-apa ya</em>
+    Bakso eta gg
+    </p>
+  },
+  image_url: "bakso_malang.jpg",
+  price: 15000.00
+)
+
+Food.create!(
+  name: "Mie Ayam",
+  description:
+  %{
+    <p>
+    Mie + Ayam = Mie-nya dimakan ayam
+    </p>
+  },
+  image_url: "mie_ayam.jpg",
+  price: 20000.00
+)
+
+Food.create!(
+  name: "Soto Betawi",
+  description:
+  %{
+    <p>
+    Soto + Betawi = Soto Betawi
+    </p>
+  },
+  image_url: "soto_betawi.jpg",
+  price: 16000.00
+)
