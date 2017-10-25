c124195b1b80c66041a343cfc3a7d687c6f35d37
Tira Sundara <sundaralinus@gmail.com>
2017-10-25 15:58:45 +0700

    Preparation Test Data

--- spec/factories/food.rb
+++ spec/factories/food.rb
@@ -1,8 +1,14 @@
 FactoryGirl.define do
   factory :food do
     name {Faker::Food.dish}
-    description "Betawi........"
+    description { Faker::Food.ingredient }
     price 10000.0
     image_url "nasi_kuning.png"
+  end
+
+  factory :invalid_food, parent: :food do
+    name nil
+    description nil
+    price 10000.0
   end
 end
