aed274207a36857a752c9c67b837822b3a2ccb46
Tira Sundara <sundaralinus@gmail.com>
2017-10-27 14:32:47 +0700

    Add smarter Cart part 1

--- spec/models/cart_spec.rb
+++ spec/models/cart_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Cart, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before :each do
+    @food = create(:food, name: 'Sama')
+    @cart = Cart.new
+  end
+  # pending "add some examples to (or delete) #{__FILE__}"
+  it "does not change the number of line item if the same food is added" do
+    cart = create(:cart)
+    food = create(:food, name: 'Nasi Uduk')
+    line_item = create(:line_item, food: food, cart: cart)
+    expect { cart.add_food(food) }.not_to change(LineItem, :count)
+  end
+  it "increment the qty of line_item if the same food is added" do
+    cart = create(:cart)
+    food = create(:food, name: 'Nasi Uduk')
+    line_item = create(:line_item, food: food, cart: cart)
+    expect(cart.add_food(food).quantity).to eq(2)
+    #expect { cart.add_food(food) }.to change(line_item.quantity, :count)
+  end
 end
