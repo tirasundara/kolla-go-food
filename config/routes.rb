ccf4e1b2b4c3734856278cd9427dde6a960b92b3
Tira Sundara <sundaralinus@gmail.com>
2017-11-02 17:18:20 +0700

    Add SessionsController to handle login

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,25 @@
 Rails.application.routes.draw do
-  resources :carts
-  root 'store#index', as: 'store_index'
-  # get 'store/index'
 
+  get 'admin/index', as: 'admin'
+
+  root 'store#index', as: 'store_index'
+
+  controller :sessions do
+    get 'login' => :new
+    post 'login' => :create
+    delete 'logout' => :destroy
+  end
+
+  resources :carts
   resources :buyers
   resources :foods
   resources :line_items
   resources :categories
   resources :orders
   resources :users
-  get 'home/hello'
-  get 'home/goodbye'
+
+  # get 'home/hello'
+  # get 'home/goodbye'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
